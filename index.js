38314f2d57b122bc64ff393bbf79b4d2c50eb1e0
Xavha <xavhaswha@gmail.com>
2024-03-04 18:53:35 +0700

    edit function

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
 const express = require('express')
 const app = express()
 
-app.get('/is_prime/:num', (req,res) => {
-    const num = parseInt(req.params.num)
-
-    if(num===1){
-        console.log(`1`)
-        return res.send('false')
+function is_prime(num){
+    if(num<=1){
+        // console.log(`1`)
+        return false
     }
 
     for(i=2;i<num;i++){
         if(num%i===0){
-            console.log(`${num} false`)
-            return res.send('false')
+            // console.log(`${num} false`)
+            return false
         }
     }
-    console.log(`${num} true`)
-    return res.send('true')
+    // console.log(`${num} true`)
+    return true
+}
+
+app.get('/is_prime/:num', (req,res) => {
+    let num = parseInt(req.params.num)
+    const result = is_prime(num);
+    res.send((result).toString());
+
 })
 
-app.listen(3000, () => {
+const server = app.listen(3000, () => {
     console.log("Server run 3000")
 })
 
-module.exports = app
+module.exports = { server , is_prime }
