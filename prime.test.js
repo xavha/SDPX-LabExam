5f69cd44c5a5995113d847f6d84f6254f0b9b176
Xavha <xavhaswha@gmail.com>
2024-03-04 20:07:41 +0700

    create Dockerfile

--- prime.test.js
+++ prime.test.js
@@ -1,17 +1,2 @@
-const {is_prime , server} = require('./index');
-
-afterAll(done => {
-    server.close(done);
-});
-
-describe('test is prime', () => {
-    test('true_when_x_is_17',() => {
-        expect(is_prime(17)).toBe(true);
-    });
-    test('false_when_x_is_36',() => {
-        expect(is_prime(36)).toBe(false);
-    });
-    test('true_when_x_is_13219',() => {
-        expect(is_prime(13219)).toBe(true);
-    });
-});
+const app = require('./index');
+const request = require('supertest');
