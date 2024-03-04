210578752fc533d993ee5492b85f0e1b2cd74b74
Xavha <xavhaswha@gmail.com>
2024-03-04 20:39:22 +0700

    edit unit test

--- prime.test.js
+++ prime.test.js
@@ -1,2 +1,17 @@
-const app = require('./index');
-const request = require('supertest');
+const { is_prime, server } = require('./index');
+
+afterAll(done => {
+  server.close(done);
+});
+
+test('true_when_x_is_17', () => {
+  expect(is_prime(17)).toBe(true);
+});
+
+test('false_when_x_is_36', () => {
+  expect(is_prime(36)).toBe(false);
+});
+
+test('true_when_x_is_13219', () => {
+  expect(is_prime(13219)).toBe(true);
+});
